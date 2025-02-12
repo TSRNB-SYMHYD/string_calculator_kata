637923ef72f4cee6614580a5f44ab566e1e3723c
TSRNB-SYMHYD <nithin.bharadwaj@symphonize.com>
2025-02-12 17:45:07 +0530

    Implement add() to support custom delimiter syntax ('//[delimiter]\\n...')

--- lib/string_calculator_kata.dart
+++ lib/string_calculator_kata.dart
@@ -4,6 +4,14 @@ int add(String numbers) {
     return int.parse(numbers);
   }
   
-  final parts = numbers.split(RegExp(r",|\n"));
+  String delimiterPattern = r",|\n";
+  if (numbers.startsWith("//")) {
+    final delimiterEndIndex = numbers.indexOf("\n");
+    final customDelimiter = numbers.substring(2, delimiterEndIndex);
+    delimiterPattern = RegExp.escape(customDelimiter);
+    numbers = numbers.substring(delimiterEndIndex + 1);
+  }
+  
+  final parts = numbers.split(RegExp(delimiterPattern));
   return parts.map(int.parse).reduce((a, b) => a + b);
 }
