8308aba627ccc6c5f9190180134e77e756e35196
TSRNB-SYMHYD <nithin.bharadwaj@symphonize.com>
2025-02-12 17:29:54 +0530

    Implement add() for empty string: return 0

---
+++ lib/string_calculator_kata.dart
@@ -0,0 +1,4 @@
+int add(String numbers) {
+  if (numbers.isEmpty) return 0;
+  return 1; //placeholder to return non-null as the function had definite return type
+}
--- test/string_calculator_kata_test.dart
+++ test/string_calculator_kata_test.dart
@@ -1,3 +1,4 @@
+import 'package:string_calculator_kata/string_calculator_kata.dart';
 import 'package:test/test.dart';
 
 void main() {
